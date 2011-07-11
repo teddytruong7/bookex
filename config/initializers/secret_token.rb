d9a14e179c073c7fd059114246d4c73fe05a229e
Teddy Truong <teddytruong7@gmail.com>
2011-07-11 13:44:14 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookex::Application.config.secret_token = '2423c4b5f0f4ca1142c828e41a5f24bf8567c7a673955086f0e106991c7b61885b1512193b5182bc84ee3d192cc9a9932c46f6cbd9f9aeec17139929dd8c8a12'
