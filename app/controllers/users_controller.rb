ea6438fb481e8cce2fc974a393b5340b20a9f549
Teddy Truong <teddytruong7@gmail.com>
2011-07-11 21:35:33 -0700

    Signup is complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
     @title = "Sign up"
   end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+  
+  def create
+    @user = User.new(params[:id])
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Online Book Exchange!"
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 
 end
